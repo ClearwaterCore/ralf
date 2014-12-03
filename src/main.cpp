291be4c34a579b63ade23d00775e98e4daade9c5
David Collins <david.collins@metaswitch.com>
2014-12-03 15:10:57 -0600

    removed signalnames as per review comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /**
- * @file main.cpp main function for homestead
+ * @file main.cpp main function for ralf
  *
  * Project Clearwater - IMS in the Cloud
  * Copyright (C) 2013  Metaswitch Networks Ltd
@@ -38,6 +38,8 @@
 #include <signal.h>
 #include <semaphore.h>
 #include <strings.h>
+
+#include "ralf_ent_definitions.h"
 
 #include "ipv6utils.h"
 #include "memcachedstore.h"
@@ -185,7 +187,7 @@ int init_options(int argc, char**argv, struct options& options)
       }
       else
       {
-        LOG_WARNING("Invalid --sas option, SAS disabled\n");
+	CL_RALF_INVALID_SAS_OPTION.log();
       }
     }
     break;
@@ -221,7 +223,7 @@ int init_options(int argc, char**argv, struct options& options)
       return -1;
 
     default:
-      LOG_ERROR("Unknown option: %d.  Run with --help for options.\n", opt);
+      CL_RALF_INVALID_OPTION_C.log();
       return -1;
     }
   }
@@ -245,6 +247,8 @@ void exception_handler(int sig)
   signal(SIGSEGV, SIG_DFL);
 
   // Log the signal, along with a backtrace.
+  CL_RALF_CRASHED.log();
+  closelog();
   LOG_BACKTRACE("Signal %d caught", sig);
 
   // Ensure the log files are complete - the core file created by abort() below
@@ -278,8 +282,12 @@ int main(int argc, char**argv)
   options.sas_server = "0.0.0.0";
   options.sas_system_name = "";
 
+  openlog("ralf", PDLOG_PID, PDLOG_LOCAL6);
+  CL_RALF_STARTED.log();
+
   if (init_logging_options(argc, argv, options) != 0)
   {
+    closelog();
     return 1;
   }
 
@@ -380,6 +388,7 @@ int main(int argc, char**argv)
   }
   catch (HttpStack::Exception& e)
   {
+    CL_RALF_HTTP_ERROR.log(e._func, e._rc);
     fprintf(stderr, "Caught HttpStack::Exception - %s - %d\n", e._func, e._rc);
   }
 
@@ -401,6 +410,7 @@ int main(int argc, char**argv)
 
   sem_wait(&term_sem);
 
+  CL_RALF_ENDED.log();
   try
   {
     http_stack->stop();
@@ -408,6 +418,7 @@ int main(int argc, char**argv)
   }
   catch (HttpStack::Exception& e)
   {
+    CL_RALF_HTTP_STOP_ERROR.log(e._func, e._rc);
     fprintf(stderr, "Caught HttpStack::Exception - %s - %d\n", e._func, e._rc);
   }
 
@@ -419,6 +430,7 @@ int main(int argc, char**argv)
 
   delete load_monitor; load_monitor = NULL;
 
+  closelog();
   signal(SIGTERM, SIG_DFL);
   sem_destroy(&term_sem);
 }
